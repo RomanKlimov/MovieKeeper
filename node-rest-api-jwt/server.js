4ff4127715b1d6df0f32cbc0db2a7eb00bdb914a
Roman <itis.klimov@gmail.com>
2018-07-10 23:49:27 +0300

    rest server sceleton

---
+++ node-rest-api-jwt/server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Roman on 10.07.2018.
+ */
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.get('/', function(req, res){
+    res.json({"tutorial" : "Build REST API with node.js"});
+});
+app.listen(3000, function(){ console.log('Node server listening on port 3000');});
